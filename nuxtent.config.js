c3bad181cab60365a61bbef8ca82aae7fdbf5064
David Royer <droyer01@gmail.com>
2018-03-29 01:47:21 -0400

    testing config again

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -9,24 +9,17 @@ module.exports = {
         'getAll'
     ]
   },
-  api: {
-    baseURL: process.env.NODE_ENV === 'production'
-            ? 'http://nuxtent-example.netlify.com'
-            : 'http://localhost:3000',
-    browserBaseURL: process.env.NODE_ENV === 'production'
-            ? 'http://nuxtent-example.netlify.com'
-            : 'http://localhost:3000'
+  // api: {
+  //   baseURL: 'http://localhost:3000',
+  //   browserBaseURL: 'http://nuxtent-example.netlify.com'
+  // },
+
+
+
+  api: function(isStatic) {
+    return {
+      baseURL: 'http://localhost:3000',
+      browserBaseURL: isStatic ? 'http://nuxtent-example.netlify.com' : ''
+    }
   }
-
-
-
-  // api: function(isStatic) {
-  //   return {
-  //     baseURL:
-  //       process.env.NODE_ENV === 'production'
-  //         ? 'http://nuxtent-example.netlify.com'
-  //         : 'http://localhost:3000',
-  //     browserBaseURL: isStatic ? 'http://nuxtent-example.netlify.com' : ''
-  //   }
-  // }
 }
